b4f16ef3f995a68acd741049984a8eef9d6cb586
mauro <mauro@mauro-PC>
2021-02-04 15:44:59 -0300

    alterações gerais

--- ClienteHttpConsumer/src/main/java/br/com/stu/cliente/ClienteWeb.java
+++ ClienteHttpConsumer/src/main/java/br/com/stu/cliente/ClienteWeb.java
@@ -8,10 +8,10 @@ import org.apache.http.client.fluent.Request;
 public class ClienteWeb {
     public static void main(String[] args) {
 	try {
-	    String retorno = Request.Post("http://localhost:9000/gerenciador/empresas").addHeader("Accept", "application/xml").execute().returnContent().asString();
+	    String retorno = Request.Post("http://localhost:9000/gerenciador/empresas").addHeader("Accept", "application/json").execute().returnContent().asString();
 	    System.out.println(retorno);
 
-	    retorno = Request.Post("http://localhost:9000/gerenciador/empresas").addHeader("Accept", "application/json").execute().returnContent().asString();
+	    retorno = Request.Post("http://localhost:9000/gerenciador/empresas").addHeader("Accept", "application/xml").execute().returnContent().asString();
 	    System.out.println(retorno);
 
 	    retorno = Request.Post("http://localhost:9000/gerenciador/empresas").addHeader("Accept", "text/html").execute().returnContent().asString();
